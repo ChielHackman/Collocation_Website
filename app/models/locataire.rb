6202b531d655909dc44d61655ae8a01d6dbfec25
Chiel <c.hackman@cevesvergeer.nl>
2018-07-27 16:11:18 +0200

    Setup the models to compare and create new records

--- app/models/locataire.rb
+++ app/models/locataire.rb
@@ -7,8 +7,22 @@ class Locataire < ApplicationRecord
     validates :age, presence:true
     validates :other, presence:true
     validates :loyer, presence:true
-    
+
+    # After a form for a new locataire is submitted call the compare_forms method
+    after_create :compare_forms
+
+    def compare_forms
+      # Put all owners in a variable so you can compare the filled in form with each of the owners
+      owners = Owner.all
+
+      # For each owner check if the age and sexe is equal to the age and sexe from the just created locataire and if so, create a new FromMatcher with the corresponding id's
+      # FromMatcher should be FormMatcher and inside the FromMatcher table you want to change locataires_id to locataire_id because it represents one single locataire
+      # Will prevent you from making mistakes in the future :-)
+      owners.each do |owner|
+        if owner.age == age && owner.sexe == sexe
+          FromMatcher.create(locataire_id: id, owner_id: owner.id)
+        end
+      end
+    end
 
 end
-
-
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -12,6 +12,21 @@ class Owner < ApplicationRecord
     validates :bail, presence:true
     validates :comments, presence:true
 
-    
-end
+    # After a form for a new owner is submitted call the compare_forms method
+    after_create :compare_forms
 
+    def compare_forms
+      # Put all locataires in a variable so you can compare the filled in form with each of the locataires
+      locataires = Locataire.all
+
+      # For each locataire check if the age and sexe is equal to the age and sexe from the just created owner and if so, create a new FromMatcher with the corresponding id's
+      # FromMatcher should be FormMatcher and inside the FromMatcher table you want to change locataires_id to locataire_id because it represents one single locataire
+      # Will prevent you from making mistakes in the future :-)
+      locataires.each do |locataire|
+        if locataire.age == age && locataire.sexe == sexe
+          FromMatcher.create(owner_id: id, locataires_id: locataire.id)
+        end
+      end
+    end
+
+end
